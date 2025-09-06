713ab5f8b076eaac2c6d767f353a93f9966ea791
DTH235688_VoAnhKiet <kietanhvo312@gmail.com>
2025-09-06 14:39:15 +0700

    Delete bài10.py

--- bài10.py
+++
@@ -1,8 +0,0 @@
-print('            *               \n')
-print('          * * *              \n')
-print('      * * * * * * *            \n')
-print('          * * *              \n')
-print('        * * * * *            \n')
-print('  * * * * * * * * * * *           \n')
-print('          *   *            \n')
-print('          *   *            \n')
